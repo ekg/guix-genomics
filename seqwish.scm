14fe6450d6dcb82f81dfe40f14d97cdaffe0e933
Erik Garrison <erik.garrison@gmail.com>
2020-01-14 14:26:04 +0100

    seqwish package

---
+++ seqwish.scm
@@ -0,0 +1,51 @@
+(define-module (seqwish)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system cmake)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages wget)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages compression))
+
+(define-public seqwish
+  (let ((version "v0.2.0")
+        (commit "209d0bcb419049f7adddb2e976d52b20d6e17605")
+        (package-revision "1"))
+    (package
+     (name "seqwish")
+     (version (string-append version "+" (string-take commit 7) "-" package-revision))
+     (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/ekg/seqwish.git")
+                    (commit commit)
+                    (recursive? #t)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1cffzvqkp323gdypg44zwgbcbpi4rspxrz9wbs1h3zv91xgmb2np"))))
+     (build-system cmake-build-system)
+     (arguments
+      `(#:phases
+        (modify-phases
+         %standard-phases
+         (delete 'check))))
+     (native-inputs
+      `(("gcc" ,gcc-9)
+        ("cmake" ,cmake)
+        ("zlib" ,zlib)))
+     (synopsis "variation graph inducer")
+     (description
+"seqwish implements a lossless conversion from pairwise alignments between
+sequences to a variation graph encoding the sequences and their alignments.  As
+input we typically take all-versus-all alignments, but the exact structure of
+the alignment set may be defined in an application specific way.  This algorithm
+uses a series of disk-backed sorts and passes over the alignment and sequence
+inputs to allow the graph to be constructed from very large inputs that are
+commonly encountered when working with large numbers of noisy input sequences.")
+     (home-page "https://github.com/ekg/freebayes")
+     (license license:expat))))
