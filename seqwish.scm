4ebeda3cdbd8f24c85cf8057bfb4f3e934359bdb
Erik Garrison <erik.garrison@gmail.com>
2020-06-09 03:52:41 -0500

    Revert "Introduce -D_GLIBCXX_USE_C99_STDLIB switch that compiles sdsl-line and seqwish"
    This reverts commit 74712cdddd4b15ffde5059e21a7776578cd139f9.

--- seqwish.scm
+++ seqwish.scm
@@ -1,67 +1,15 @@
 (define-module (seqwish)
   #:use-module (guix packages)
   #:use-module (guix git-download)
-  #:use-module (guix download)
   #:use-module (guix build-system cmake)
   #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (gnu packages)
   #:use-module (gnu packages base)
-  #:use-module (gnu packages datastructures)
-  #:use-module (gnu packages documentation)
   #:use-module (gnu packages gcc)
-  #:use-module (gnu packages graphviz)
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages python)
   #:use-module (gnu packages wget)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages compression))
-
-(define-public sdsl-lite-git
-  (let ((commit "c32874cb2d8524119f25f3b501526fe692df29f4"))
-   (package
-    (name "sdsl-lite-git")
-    (version (string-append "2.1.2-pre-" (string-take commit 7)))
-    (source (origin
-               (method git-fetch)
-                (uri (git-reference
-                 (url "https://github.com/simongog/sdsl-lite.git") 
-                 (commit commit))) 
-                 (file-name (string-append name "-" version "-checkout"))
-                 (sha256
-                  (base32
-                    "1p53cgrgkp72s0mx262pxz90mf04vy4c1189xlx146qh8fznywg4"))
-                (modules '((guix build utils)))
-                (snippet
-                 '(begin
-                  (delete-file-recursively "external") #t))
-                (patches (list
-                   (search-patch "sdsl-lite-git.patch")))))
-    (build-system cmake-build-system)
-    (arguments
-     '(#:phases
-       (modify-phases %standard-phases
-         (add-after 'install 'install-static-library
-           (lambda* (#:key outputs #:allow-other-keys)
-             (let ((out (assoc-ref outputs "out")))
-               (copy-file "lib/libsdsl_static.a"
-                          (string-append out "/lib/libsdsl.a")))
-             #t)))))
-    (inputs
-     `(("doxygen" ,doxygen)
-       ("graphviz" ,graphviz)))
-    (native-inputs                                                                                         
-     `(("libdivsufsort" ,libdivsufsort)))
-    (home-page "https://github.com/simongog/sdsl-lite")
-    (synopsis "Succinct data structure library")
-    (description "The Succinct Data Structure Library (SDSL) is a powerful and
-flexible C++11 library implementing succinct data structures.  In total, the
-library contains the highlights of 40 research publications.  Succinct data
-structures can represent an object (such as a bitvector or a tree) in space       
-close to the information-theoretic lower bound of the object while supporting     
-operations of the original object efficiently.  The theoretical time              
-complexity of an operation performed on the classical data structure and the      
-equivalent succinct data structure are (most of the time) identical.")            
-    (license license:gpl3+))))
 
 (define-public seqwish
   (let ((version "0.6.0")
@@ -79,19 +27,15 @@ equivalent succinct data structure are (most of the time) identical.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1g3k0bbdfnbap2xw27js2yc2xqs96vxarmmg2hv0zx62hfjkz7lx"))
-              (patches (list
-                 (search-patch "seqwish.patch")))))
+                "1g3k0bbdfnbap2xw27js2yc2xqs96vxarmmg2hv0zx62hfjkz7lx"))))
      (build-system cmake-build-system)
      (arguments
       `(#:phases
         (modify-phases
          %standard-phases
          (delete 'check))))
-     (inputs
-      `(("sdsl-lite-git" ,sdsl-lite-git)))
      (native-inputs
-      `(("gcc" ,gcc)
+      `(("gcc" ,gcc-9)
         ("cmake" ,cmake)
         ("zlib" ,zlib)))
      (synopsis "variation graph inducer")
