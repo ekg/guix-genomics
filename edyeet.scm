2bea373a2b3b630a801aa7bad3b3982a17ef5cbb
Erik Garrison <erik.garrison@gmail.com>
2020-10-30 12:20:46 -0500

    update edyeet

--- edyeet.scm
+++ edyeet.scm
@@ -18,7 +18,7 @@
 
 (define-public edyeet
   (let ((version "v0.1")
-        (commit "b62b3896ccf74d322e8b7e8f0e4279582e80c424")
+        (commit "63663eb033e67accb01e75c9bdd18ab3e0cd3cc6")
         (package-revision "1"))
     (package
      (name "edyeet")
@@ -32,7 +32,7 @@
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0yryar527bxy3q1iggzcsv3f5nf29jj5zzf2h1prw8lmiiks2sfw"))))
+                "1h5rfq1pq21c4hc8bbzhbrmd3shnsigxk0ngsp0y0dp2x5701kwm"))))
      ;(patches (search-patches "mashmap-make-the-aligner-as-well.patch"))))
      (build-system gnu-build-system)
      (arguments
