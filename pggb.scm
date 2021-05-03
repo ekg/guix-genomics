9533b22fbca5aec9244d0ab2d3da7204cd6840cc
Erik Garrison <erik.garrison@gmail.com>
2021-05-03 17:30:30 +0200

    add nonfunctional script for pggb

---
+++ pggb.scm
@@ -0,0 +1,37 @@
+(define-module (wfmash)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system cmake)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages))
+
+(define-public pggb
+  (let ((version "0.1.0")
+        (commit "8c915ee4f1c1917de31b4221752be58c00ff0a76")
+        (package-revision "1"))
+    (package
+     (name "pggb")
+     (version (string-append version "+" (string-take commit 7) "-" package-revision))
+     (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/pangenome/pggb.git")
+                    (commit commit)
+                    (recursive? #f)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0izip35a0xd8r018p1smfxpiwlhpva1aqqfi23xwck854xgibw4n"))))
+     (build-system copy-build-system)
+     (arguments
+      '(#:install-plan
+        '(("pggb" "bin/"))))
+     (synopsis "the pangenome graph builder"
+     (description "This pangenome graph construction pipeline renders
+a collection of sequences into a pangenome graph (in the variation
+graph model).  Its goal is to build a graph that is locally directed
+and acyclic while preserving large-scale variation.  Maintaining local
+linearity is important for the interpretation, visualization, and
+reuse of pangenome variation graphs.")
+     (home-page "https://github.com/pangenome/pggb")
+     (license license:expat))))
