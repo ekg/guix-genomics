398169972471fcb17a4b1638ea095dc49f0ab353
Efraim Flashner <efraim@flashner.co.il>
2021-05-11 11:31:52 +0300

    paf2dot: Use wrap script with helpful binaries.

--- paf2dot.scm
+++ paf2dot.scm
@@ -1,10 +1,13 @@
 (define-module (paf2dot)
   #:use-module (guix packages)
   #:use-module (guix git-download)
-  #:use-module (guix build-system copy)
+  #:use-module (guix build-system trivial)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages)
-  #:use-module (gnu packages maths))
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages xdisorg))
 
 (define-public paf2dot
   (let ((version "0.0.0")
@@ -17,18 +20,34 @@
               (method git-fetch)
               (uri (git-reference
                     (url "https://github.com/pangenome/paf2dot.git")
-                    (commit commit)
-                    (recursive? #f)))
+                    (commit commit)))
               (file-name (git-file-name name version))
               (sha256
                (base32
                 "0zr5jv4wjy35bk97hrcvwvi94d4cnvh5mr5230nz56ni6dgz02ic"))))
-     (build-system copy-build-system)
+     (build-system trivial-build-system)
      (arguments
-      '(#:install-plan
-        '(("paf2dot" "bin/"))))
+      `(#:modules ((guix build utils))
+        #:builder
+        (begin
+          (use-modules (guix build utils))
+          (let* ((out (assoc-ref %outputs "out"))
+                 (bin (string-append out "/bin"))
+                 (doc (string-append out "/share/doc/" ,name "-" ,version))
+                 (perl (string-append (assoc-ref %build-inputs "perl") "/bin")))
+            (install-file (string-append (assoc-ref %build-inputs "source") "/paf2dot") bin)
+            (install-file (string-append (assoc-ref %build-inputs "source") "/README.md") doc)
+            (patch-shebang (string-append bin "/paf2dot") (list perl))
+            (wrap-script (string-append out "/bin/paf2dot")
+                         #:guile (string-append (assoc-ref %build-inputs "guile") "/bin/guile")
+              `("PATH" ":" prefix (,(string-append (assoc-ref %build-inputs "gnuplot") "/bin")
+                                   ,(string-append (assoc-ref %build-inputs "xclip") "/bin"))))
+            #t))))
      (inputs
-      `(("gnuplot" ,gnuplot)))
+      `(("gnuplot" ,gnuplot)
+        ("guile" ,guile-3.0)
+        ("perl" ,perl)
+        ("xclip" ,xclip)))
      (synopsis "visualize alignments in PAF format using gnuplot")
      (description "Use gnuplot to generate a dotplot from sequence
 alignments in PAF format. Only the alignment / mapping start and end
